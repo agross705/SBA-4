c78bb1844714e1831595deb124b4dcc81bc31857
agross705 <a.gross705@gmail.com>
2026-01-15 20:38:19 -0500

    Stored each task as an object with properties

---
+++ app.js
@@ -0,0 +1,5 @@
+// DOM Elements
+const taskNameInput = document.getElementById("taskName");
+const taskCategoryInput = document.getElementById("taskCategory");
+const taskDeadlineInput = document.getElementById("taskDeadline");
+const taskStatusInput = document.getElementById("taskStatus");
